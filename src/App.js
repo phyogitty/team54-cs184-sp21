6b7bf50deb1bbc15cba207384b3e230bc09f65b9
Phyo Khine <pkhine.2020@berkeley.edu>
2021-05-11 15:22:43 -0700

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import TechnicalApproach from "./components/TechnicalApproach";
 import References from "./components/References";
 import Contribution from "./components/Contribution";
 import Results from "./components/Results";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
@@ -15,6 +16,12 @@ function App() {
       <TopNav />
       {/* <div id="topNav-area"></div> */}
       <Title />
+      <div className="center">
+        {" "}
+        <ReactPlayer url="https://youtu.be/ZZIFtwRmq9o" />
+      </div>
+
+      <div className="py-5" id="next-abstract"></div>
       <Abstract />
       <TechnicalApproach />
       <Results />
